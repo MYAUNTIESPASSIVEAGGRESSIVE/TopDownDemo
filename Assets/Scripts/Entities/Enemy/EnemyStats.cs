bdc85e547147d08863a40f49585e61a8e4b961d5
MYAUNTIESPASSIVEAGGRESSIVE <adamkw123lightning@gmail.com>
2024-11-16 23:18:39 +0000

    Guns

--- Assets/Scripts/Entities/Enemy/EnemyStats.cs
+++ Assets/Scripts/Entities/Enemy/EnemyStats.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class EnemyStats : EntityStats
 {
+    public GameObject Target;
+    public float EnemyDamage = 15f;
+    public float EnemySpeed = 5f;
+    public float EnemyAttackRange = 1f;
+
     protected override void Start()
     {
         base.Start();
+    }
+
+    protected void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, EnemySpeed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, Target.transform.position) > EnemyAttackRange)
+        {
+            EnemyAttackTarget();
+        }
     }
 
     protected override void DeathLogic()
@@ -15,8 +30,11 @@ public class EnemyStats : EntityStats
 
         CurrentHealth = 0;
 
-
         IsDead = true;
+    }
 
+    private void EnemyAttackTarget()
+    {
+        //do enemy attacking stuff.
     }
 }
--- Assets/Scripts/Entities/EntityStats.cs
+++ Assets/Scripts/Entities/EntityStats.cs
@@ -6,9 +6,8 @@ using UnityEngine.AI;
 
 public class EntityStats : MonoBehaviour
 {
-    protected NavMeshAgent MyNav;
-    protected CircleCollider2D MyCol;
-    protected Animator MyAnim;
+    protected CircleCollider2D EntityCol;
+    protected Animator EntityAnim;
 
     public float MaxHealth;
     protected float CurrentHealth;
@@ -19,9 +18,8 @@ public class EntityStats : MonoBehaviour
 
     protected virtual void Start()
     {
-        TryGetComponent(out MyNav);
-        TryGetComponent(out MyCol);
-        TryGetComponent(out MyAnim);
+        TryGetComponent(out EntityCol);
+        TryGetComponent(out EntityAnim);
 
         CurrentHealth = MaxHealth;
     }
